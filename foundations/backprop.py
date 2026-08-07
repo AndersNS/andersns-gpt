c9c0ddcea516c9ebd46925ebc1f652588dc79648
Anders N. Slinde <AndersNS@users.noreply.github.com>
2026-08-07 14:03:47 +0200

    Update Backpropagation (Build a Neural Net 2/5)

--- foundations/backprop.py
+++ foundations/backprop.py
@@ -9,18 +9,27 @@ class Solution:
         # w: 1D weight array
         # b: scalar bias
         # y_true: true target value
-        #
-        # Forward: z = dot(x, w) + b, y_hat = sigmoid(z)
-        # Loss: L = 0.5 * (y_hat - y_true)^2
-        # Return: (dL_dw rounded to 5 decimals, dL_db rounded to 5 decimals)
 
+
+        # Forward: z = dot(x, w) + b, y_hat = sigmoid(z)
         z = np.dot(x, w) + b
-        y_hat = 1.0 / (1.0 + np.exp(-z)) # sigmoid
+        y_hat = 1.0 / (1.0 + np.exp(-z))  
+
+        # Loss: L = 0.5 * (y_hat - y_true)^2
+
+        # Gradient:
         error = y_hat - y_true
         sigmoid_deriv = y_hat * (1.0 - y_hat)
-
-
-        delta = error * sigmoid_deriv
-        grad_w = np.round(x * delta, 5)
-        grad_b = round(float(delta), 5)
+        
+        # delta is the overall derivative of the loss function:
+        dl_dz = error * sigmoid_deriv
+        
+        # dl_dw is the derivative of the loss function with respect to each weight
+        dl_dw = x * dl_dz
+        # dl_db is the derivative of the loss runction with respect to the scalar bias.
+        # for the sigmoid function, it is the same as dl_dz
+        
+        # Return: (dL_dw rounded to 5 decimals, dL_db rounded to 5 decimals)
+        grad_w = np.round(dl_dw, 5)
+        grad_b = round(float(dl_dz), 5)
         return (grad_w, grad_b)
